20492f8715bafe02bd495d9f9641c05990ab8be5
Angel Vazquez Ruiz <anngelruiz@hotmail.com>
2024-06-21 21:15:12 -0600

    First commit

---
+++ src/main/java/com/ika_climbing/IkaClimbingApplication.java
@@ -0,0 +1,13 @@
+package com.ika_climbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkaClimbingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkaClimbingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ika_climbing/IkaClimbingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ika_climbing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkaClimbingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
